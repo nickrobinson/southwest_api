7ebb3a194e2ee8dcb6b24ee2bcc680766016b679
Nick Robinson <nr33@msstate.edu>
2015-08-02 10:26:02 -0400

    Wrapping in lambda handler

--- cron/cookie.js
+++ cron/cookie.js
@@ -1,40 +1,46 @@
 var request = require('request');
 var AWS = require('aws-sdk'); 
 
-var options = {
-    url: 'https://www.southwest.com/',
-    followAllRedirects: true,
-    jar: true,
-    method: 'GET',
-    headers: {
-	'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:39.0) Gecko/20100101 Firefox/39.0',
-	'Host': 'www.southwest.com',
-	'Referer': 'https://www.southwest.com/flight/?clk=GSUBNAV-AIR-BOOK',
-	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-	'Connection': 'keep-alive'
-    }
-};
-
-request(options, function(error, response, body){
-    console.log(response.headers['set-cookie']);
-
-    var dynamodb = new AWS.DynamoDB(options = {'region': 'us-east-1'});
-
-    var params = {
-	Item: {
-	    id: {
-		N: '1'
-	    },
-	    cookie: {
-		SS: response.headers['set-cookie'] 
-	    }
-	},
-	TableName: 'cookies',
+exports.handler = function(event, context) {
+    var options = {
+	url: 'https://www.southwest.com/',
+	followAllRedirects: true,
+	jar: true,
+	method: 'GET',
+	headers: {
+	    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:39.0) Gecko/20100101 Firefox/39.0',
+	    'Host': 'www.southwest.com',
+	    'Referer': 'https://www.southwest.com/flight/?clk=GSUBNAV-AIR-BOOK',
+	    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	    'Connection': 'keep-alive'
+	}
     };
 
-    dynamodb.putItem(params, function(err, data) {
-	if (err) console.log(err, err.stack);
-	else console.log(data);
-    });
-});
+    request(options, function(error, response, body){
+	console.log(response.headers['set-cookie']);
 
+	var dynamodb = new AWS.DynamoDB(options = {'region': 'us-east-1'});
+
+	var params = {
+	    Item: {
+		id: {
+		    N: '1'
+		},
+		cookie: {
+		    SS: response.headers['set-cookie'] 
+		}
+	    },
+	    TableName: 'cookies',
+	};
+
+	dynamodb.putItem(params, function(err, data) {
+	    if (err) {
+		context.fail(err);
+	    }
+	    else {
+		context.succeed(data);
+	    }
+	});
+    });
+
+};
